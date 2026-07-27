308f85dfcc6972ab1c6d570e642ba6a4a3984159
R <github.zap510@passmail.net>
2026-07-27 21:15:35 +0200

    staging linting

--- job_postings/models/staging/adzuna/stg_adzuna__category_pulls.sql
+++ job_postings/models/staging/adzuna/stg_adzuna__category_pulls.sql
@@ -5,10 +5,10 @@ with source as (
 final as (
     select
         -- strings: trim + null-standardize
-        nullif(trim(category_tag), '')              as category_tag,
-        nullif(trim(category_label), '')            as category_label,
-        mean                                        as adzuna_reported_mean_salary,
-        count                                       as adzuna_total_count
+        mean as adzuna_reported_mean_salary,
+        count as adzuna_total_count,
+        nullif(trim(category_tag), '') as category_tag,
+        nullif(trim(category_label), '') as category_label
     from source
 )
 
--- job_postings/models/staging/adzuna/stg_adzuna__jobs.sql
+++ job_postings/models/staging/adzuna/stg_adzuna__jobs.sql
@@ -5,34 +5,35 @@ with source as (
 parsed as (
     select
         *,
-        from_json(nullif(trim(location_area), ''), '["VARCHAR"]') as location_area_parsed
+        from_json(nullif(trim(location_area), ''), '["VARCHAR"]')
+            as location_area_parsed
     from source
 ),
 
 final as (
     select
         -- strings: trim + null-standardize
-        nullif(trim(job_id), '')     as job_id,
-        nullif(trim(title), '')     as job_title,
-        nullif(trim(company), '')     as company,
-        nullif(trim(location_display), '')     as location_display,
-        nullif(trim(location_area_parsed[1] ), '')              as country,
-        nullif(trim(location_area_parsed[1] ), '')              as region,
-        nullif(trim(location_area_parsed[1] ), '')              as city,
-        nullif(trim(location_area_parsed[1] ), '')              as district,
-        nullif(trim(category_tag), '')              as category_tag,
-        nullif(trim(category_label), '')              as category_label,
         salary_min,
         salary_max,
-        nullif(trim(salary_is_predicted), '')::boolean              as salary_is_predicted,
-        nullif(trim(contract_time), '')              as contract_time,
-        nullif(trim(contract_type), '')              as contract_type,
-        try_cast(nullif(trim(created),'')  as timestamptz)            as posted_at,
-        nullif(trim(description), '')              as job_description,
-        nullif(trim(redirect_url), '')              as redirect_url,
-        nullif(trim(country), '')              as country_code,
-        ingested_at 
-
+        ingested_at,
+        nullif(trim(job_id), '') as job_id,
+        nullif(trim(title), '') as job_title,
+        nullif(trim(company), '') as company,
+        nullif(trim(location_display), '') as location_display,
+        nullif(trim(location_area_parsed[1]), '') as country,
+        nullif(trim(location_area_parsed[1]), '') as region,
+        nullif(trim(location_area_parsed[1]), '') as city,
+        nullif(trim(location_area_parsed[1]), '') as district,
+        nullif(trim(category_tag), '') as category_tag,
+        nullif(trim(category_label), '') as category_label,
+        nullif(trim(salary_is_predicted), '')::boolean as salary_is_predicted,
+        nullif(trim(contract_time), '') as contract_time,
+        nullif(trim(contract_type), '') as contract_type,
+        try_cast(nullif(trim(created), '') as timestamptz) as posted_at,
+        strftime(posted_at, '%Y%m%d')::int as date_key,
+        nullif(trim(description), '') as job_description,
+        nullif(trim(redirect_url), '') as redirect_url,
+        nullif(trim(country), '') as country_code
 
     from parsed
 )
