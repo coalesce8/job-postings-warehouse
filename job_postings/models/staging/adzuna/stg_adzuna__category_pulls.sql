700ca797d22428bbb373703c3a9ea43b3b2ff49e
R <github.zap510@passmail.net>
2026-07-27 08:08:50 +0200

    add category pulls staging model

---
+++ job_postings/models/staging/adzuna/stg_adzuna__category_pulls.sql
@@ -0,0 +1,16 @@
+with source as (
+    select * from {{ source('adzuna', 'raw_jobs') }}
+),
+
+final as (
+    select
+        -- strings: trim + null-standardize
+        nullif(trim(category_tag), '')              as category_tag,
+        nullif(trim(category_label), '')            as category_label,
+        mean                                        as adzuna_reported_mean_salary,
+        count                                       as adzuna_total_count,
+        ingested_at
+    from source
+)
+
+select * from final
