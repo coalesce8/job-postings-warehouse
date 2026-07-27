468dcde14de6c64e34b93cd6048613c609a19f85
R <github.zap510@passmail.net>
2026-07-27 09:35:54 +0200

    remove  ingested_at

--- job_postings/models/staging/adzuna/stg_adzuna__category_pulls.sql
+++ job_postings/models/staging/adzuna/stg_adzuna__category_pulls.sql
@@ -8,8 +8,7 @@ final as (
         nullif(trim(category_tag), '')              as category_tag,
         nullif(trim(category_label), '')            as category_label,
         mean                                        as adzuna_reported_mean_salary,
-        count                                       as adzuna_total_count,
-        ingested_at
+        count                                       as adzuna_total_count
     from source
 )
 
