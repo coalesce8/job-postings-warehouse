c94fa375b9375386770d19755e37af9f60a97852
R <github.zap510@passmail.net>
2026-07-30 08:07:58 +0200

    fix dim_company

--- job_postings/models/marts/dim_company.sql
+++ job_postings/models/marts/dim_company.sql
@@ -1,5 +1,13 @@
-with final as (
-    select company from {{ ref ('stg_adzuna__jobs') }}
+with jobs as (
+    select * from {{ ref ('stg_adzuna__jobs') }}
+),
+
+final as (
+    select
+        company_key,
+        max(company_name) as company_name
+    from jobs
+    group by company_key
 )
 
 select * from final
